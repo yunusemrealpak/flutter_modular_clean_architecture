2373c0af9d5596b3ea52d4920d26df1256121e92
yunusemrealpak <yunusemrealpak@gmail.com>
2025-10-17 20:45:08 +0300

    feat: Update core exports and clean up pubspec.yaml; remove unused test file

--- packages/shared/core/lib/core.dart
+++ packages/shared/core/lib/core.dart
@@ -1,1 +1,9 @@
+export 'package:auto_route/auto_route.dart';
+export 'package:dartz/dartz.dart' hide order, State;
+export 'package:equatable/equatable.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:freezed_annotation/freezed_annotation.dart';
+export 'package:injectable/injectable.dart' hide Order;
+export 'package:json_annotation/json_annotation.dart';
+
 export 'src/di/di_helper.dart';
