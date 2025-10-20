a71627bc2b778f450097a71f152f708942082d96
yunusemrealpak <yunusemrealpak@gmail.com>
2025-10-20 17:12:38 +0300

    feat(core): add get_it package export for dependency injection

--- packages/shared/core/lib/core.dart
+++ packages/shared/core/lib/core.dart
@@ -3,6 +3,7 @@ export 'package:dartz/dartz.dart' hide order, State;
 export 'package:equatable/equatable.dart';
 export 'package:flutter_bloc/flutter_bloc.dart';
 export 'package:freezed_annotation/freezed_annotation.dart';
+export 'package:get_it/get_it.dart';
 export 'package:injectable/injectable.dart' hide Order;
 export 'package:json_annotation/json_annotation.dart';
 
--- packages/shared/session/lib/session.dart
+++ packages/shared/session/lib/session.dart
@@ -1,11 +1,6 @@
-library;
-
-// Domain - Models
-export 'src/domain/models/session_data.dart';
-export 'src/domain/models/user_session.dart';
-
-// Presentation - BLoC
-export 'src/presentation/bloc/session_bloc.dart';
-
 // DI
 export 'src/di/injection.module.dart';
+export 'src/domain/models/session_data.dart';
+export 'src/domain/models/user_session.dart';
+// Presentation - BLoC
+export 'src/presentation/bloc/session_bloc.dart';
