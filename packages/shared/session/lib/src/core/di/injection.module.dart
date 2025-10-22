94011f7662c60d6e7359582b8f7b9d9c9f89998e
yunusemrealpak <yunusemrealpak@gmail.com>
2025-10-23 01:13:10 +0300

    fix: update import path and session_bloc reference in injection module

--- packages/shared/session/lib/src/core/di/injection.module.dart
+++ packages/shared/session/lib/src/core/di/injection.module.dart
@@ -1,4 +1,4 @@
-//@GeneratedMicroModule;SessionPackageModule;package:session/src/di/injection.module.dart
+//@GeneratedMicroModule;SessionPackageModule;package:session/src/core/di/injection.module.dart
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
 // coverage:ignore-file
@@ -8,12 +8,12 @@ import 'dart:async' as _i687;
 
 import 'package:injectable/injectable.dart' as _i526;
 import 'package:session/src/feature/presentation/bloc/session_bloc.dart'
-    as _i32;
+    as _i443;
 
 class SessionPackageModule extends _i526.MicroPackageModule {
-  // initializes the registration of main-scope dependencies inside of GetIt
+// initializes the registration of main-scope dependencies inside of GetIt
   @override
   _i687.FutureOr<void> init(_i526.GetItHelper gh) {
-    gh.lazySingleton<_i32.SessionBloc>(() => _i32.SessionBloc());
+    gh.lazySingleton<_i443.SessionBloc>(() => _i443.SessionBloc());
   }
 }
