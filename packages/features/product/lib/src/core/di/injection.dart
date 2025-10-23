3a202d07430a6524a0862c013da6f99c6940498e
yunusemrealpak <yunusemrealpak@gmail.com>
2025-10-23 10:06:20 +0300

    refactor: remove product feature package and all related files

--- packages/features/product/lib/src/core/di/injection.dart
+++
@@ -1,6 +0,0 @@
-import 'package:core/core.dart';
-
-/// Initialize dependency injection for this package
-/// This will be called from the root app's DI setup
-@InjectableInit.microPackage()
-initMicroPackage() {}
